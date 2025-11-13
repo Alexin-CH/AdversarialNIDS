9afaba109179ca95eb9a885e6cd546e885bcb70a
Alexin-CH <alexin.choisnet@gmail.com>
2025-11-13 23:35:04 +0100

    Fix logger for decision tree

--- scripts/DecisionTree.py
+++ scripts/DecisionTree.py
@@ -1,14 +1,5 @@
-# Decision Tree Classifier
-
-"""
-A decision tree algorithm is a supervised machine learning method that models decisions and their possible consequences as a tree-like structure, where each internal node represents a test on a feature, each branch represents an outcome, and each leaf node represents a class label or regression value.
-
-Parameters:
-"""
-
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import cross_val_score
-from logger import LoggerManager
 
 def train_decision_tree(X_train, y_train, max_depth=6, logger=None):
     decisionTree = DecisionTreeClassifier(max_depth=max_depth)
