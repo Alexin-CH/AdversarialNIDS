0e877dcb4f3ad1779eb207f5450bcf47707bed17
Mohand AZZOUG <zatsura2@gmail.com>
2025-11-13 12:58:01 +0100

    Import logger

--- scripts/DecisionTree.py
+++ scripts/DecisionTree.py
@@ -1,22 +1,21 @@
 # Decision Tree Classifier
+
 """
 A decision tree algorithm is a supervised machine learning method that models decisions and their possible consequences as a tree-like structure, where each internal node represents a test on a feature, each branch represents an outcome, and each leaf node represents a class label or regression value.
 
 Parameters:
-- X_train: The feature matrix (input variables) used to train the decision tree.
-- y_train: The target vector (labels) corresponding to X_train.
-- max_depth=6: Sets the maximum depth of the decision tree to 6, controlling model complexity and helping prevent overfitting.
-- cv=5: Specifies 5-fold cross-validation, meaning the data is split into 5 parts to evaluate the model’s performance more reliably.
 """
 
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import cross_val_score
+from logger import LoggerManager
 
-def train_decision_tree(X_train, y_train, max_depth=6):
+def train_decision_tree(X_train, y_train, max_depth=6, logger=None):
     decisionTree = DecisionTreeClassifier(max_depth=max_depth)
     decisionTree.fit(X_train, y_train)
     cv_decisionTree = cross_val_score(decisionTree, X_train, y_train, cv=5)
-    print('Decision Tree Classifier trained successfully.')
-    print('\nCross-validation scores:', ', '.join(map(str, cv_decisionTree)))
-    print(f'\nMean cross-validation score: {cv_decisionTree.mean():.2f}')
+    if logger is not None:
+        logger.info('Decision Tree Classifier trained successfully.')
+        logger.info('\nCross-validation scores: %s', ', '.join(map(str, cv_decisionTree)))
+        logger.info('\nMean cross-validation score: %.2f', cv_decisionTree.mean())
     return
