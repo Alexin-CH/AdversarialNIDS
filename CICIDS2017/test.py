5324923d12bfa5b35135622af8a38a5257400319
Alexin-CH <alexin.choisnet@gmail.com>
2025-11-14 11:45:15 +0100

    Fix preprocessing

---
+++ CICIDS2017/test.py
@@ -0,0 +1,16 @@
+import os
+import sys
+from datetime import datetime
+
+current_dir = os.getcwd()
+sys.path.append(current_dir)
+
+from scripts.logger import LoggerManager
+from CICIDS2017.preprocessing import preprocess_cicids2017
+
+date = datetime.now().strftime("%y%m%d_%H%M%S")
+title = "test"
+lm = LoggerManager(log_dir=f"{current_dir}/logs", log_name=title)
+lm.logger.info("Logger initialized")
+
+data = preprocess_cicids2017(logger=lm.logger)
