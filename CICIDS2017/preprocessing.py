4b6532208d03863660e000daee055e6ec129e490
Alexin-CH <alexin.choisnet@gmail.com>
2025-11-14 11:58:49 +0100

    Add data encoding

--- CICIDS2017/preprocessing.py
+++ CICIDS2017/preprocessing.py
@@ -170,3 +170,36 @@ def preprocess_cicids2017(logger=None):
         if logger:
             logger.error(f"Error during preprocessing: {e}")
         raise
+
+def data_encoding(data, logger=None):
+
+    if logger:
+        logger.info("Starting data encoding process")
+        logger.info(f"Data Labels before encoding: {data['Label'].value_counts()}")
+
+    try:
+        # Creating a dictionary that maps each label to its attack type
+        attack_map = {
+            'BENIGN': 'BENIGN',
+            'DDoS': 'DDoS',
+            'DoS Hulk': 'DoS',
+            'DoS GoldenEye': 'DoS',
+            'DoS slowloris': 'DoS',
+            'DoS Slowhttptest': 'DoS',
+            'PortScan': 'Port Scan',
+            'FTP-Patator': 'Brute Force',
+            'SSH-Patator': 'Brute Force',
+            'Bot': 'Bot',
+            'Web Attack Brute Force': 'Web Attack',
+            'Web Attack XSS': 'Web Attack',
+            'Web Attack Sql Injection': 'Web Attack',
+            'Infiltration': 'Infiltration',
+            'Heartbleed': 'Heartbleed'
+        }
+
+        # Creating a new column 'Attack Type' in the DataFrame based on the attack_map dictionary
+        data['Attack Type'] = data['Label'].map(attack_map)
+    except KeyError as e:
+        if logger:
+            logger.error(f"KeyError during data encoding: {e}")
+        raise
--- CICIDS2017/test.py
+++ CICIDS2017/test.py
@@ -6,7 +6,7 @@ current_dir = os.getcwd()
 sys.path.append(current_dir)
 
 from scripts.logger import LoggerManager
-from CICIDS2017.preprocessing import preprocess_cicids2017
+from CICIDS2017.preprocessing import preprocess_cicids2017, data_encoding
 
 date = datetime.now().strftime("%y%m%d_%H%M%S")
 title = "test"
@@ -14,3 +14,4 @@ lm = LoggerManager(log_dir=f"{current_dir}/logs", log_name=title)
 lm.logger.info("Logger initialized")
 
 data = preprocess_cicids2017(logger=lm.logger)
+data = data_encoding(data, logger=lm.logger)
