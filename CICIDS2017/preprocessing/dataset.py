e106b3fdb0652ac3535b2695932154ec7e569772
Alexin-CH <alexin.choisnet@gmail.com>
2025-11-17 17:55:52 +0100

    Use root_dir instead of current_dir

--- CICIDS2017/preprocessing/dataset.py
+++ CICIDS2017/preprocessing/dataset.py
@@ -4,8 +4,8 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 
-current_dir = os.getcwd()
-sys.path.append(current_dir)
+root_dir = os.getcwd().split("AdversarialNIDS")[0] + "AdversarialNIDS"
+sys.path.append(root_dir)
 
 from scripts.logger import SimpleLogger
 
