4d747583f87fa8bffb995cff86e5abb3193db369
ValentinMalquy <valentin.malquy@gmail.com>
2025-11-20 08:25:57 +0100

    Ajout des fonctions focal et balanced softmax qui prennent en compte la répartition des classes d'entrainement

--- scripts/losses/balanced_softmax.py
+++ scripts/losses/balanced_softmax.py
@@ -6,15 +6,13 @@ class BalancedSoftmaxLoss(nn.Module):
     """
     Balanced Softmax Cross-Entropy Loss.
     Args:
-        samples_per_class: Tensor contenant le nombre d'exemples par classe
-                           shape: (num_classes,)
+        sample_per_class : repartition of every classes in the training set
     """
     def __init__(self, samples_per_class):
         super().__init__()
         self.register_buffer("samples_per_class", torch.tensor(samples_per_class, dtype=torch.float))
 
     def forward(self, logits, targets):
-        # Ajustement des logits par log fréquences
         adjusted_logits = logits + torch.log(self.samples_per_class + 1e-12)
 
         loss = F.cross_entropy(adjusted_logits, targets)
