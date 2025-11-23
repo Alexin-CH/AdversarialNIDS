7b4a8bd175024cc440db854d06d12e1ce6d9064e
ValentinMalquy <valentin.malquy@gmail.com>
2025-11-23 15:33:04 +0100

    Changements demandés

--- NIDS_attacks/decision_tree_attack/surrogate.py
+++ NIDS_attacks/decision_tree_attack/surrogate.py
@@ -1,6 +1,6 @@
 import torch
 
-def attack_fgsm(model,criterion,x_val,y_val,top_features,eps = 0.01):
+def attack_fgsm(model,criterion,x_val,target,top_features,eps = 0.01):
     """
     Args:
         model 
@@ -15,7 +15,7 @@ def attack_fgsm(model,criterion,x_val,y_val,top_features,eps = 0.01):
     """
     x_adv = x_val.clone().detach().requires_grad_(True)
     pred = model(x_adv)
-    loss = criterion(pred,y_val)
+    loss = criterion(pred,target)
     loss.backward()
     with torch.no_grad():
         x_adv[:, top_features] = x_adv[:, top_features] + eps * x_adv.grad[:, top_features].sign()
