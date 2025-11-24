366c9ddba9ad15bac4fdf025f677140d965128e6
Alexin-CH <alexin.choisnet@gmail.com>
2025-11-24 18:33:41 +0100

    Update FGSM for Pytorch  model

--- NIDS_attacks/fgsm.py
+++ NIDS_attacks/fgsm.py
@@ -5,7 +5,7 @@ def attack_fgsm(model, criterion, x_val, target, top_features=None, eps = 0.01):
     Args:
         model 
         criterion : loss function of the model
-        x_val
+        x_val : input we want to perturb
         y_val : truth wanted
         features : features we want to perturb
         eps : range we are allowed to perturbate
@@ -16,7 +16,7 @@ def attack_fgsm(model, criterion, x_val, target, top_features=None, eps = 0.01):
     x_adv = x_val.clone().detach().requires_grad_(True)
 
     pred = model(x_adv)
-    loss = criterion(pred, target)
+    loss = criterion(pred, target) # target needs to be class indices, not one-hot. Pred is raw logits
 
     loss.backward()
     with torch.no_grad():
@@ -24,5 +24,5 @@ def attack_fgsm(model, criterion, x_val, target, top_features=None, eps = 0.01):
             x_adv = x_adv - eps * x_adv.grad.sign()
         else:
             x_adv[:, top_features] = x_adv[:, top_features] - eps * x_adv.grad[:, top_features].sign()
-    return x_adv
+    return x_adv, loss.item(), pred.detach().cpu().numpy()
     
