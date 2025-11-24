14f104d4b6dc8a39908d192f3cb6bbfd776f2306
Alexin-CH <alexin.choisnet@gmail.com>
2025-11-24 14:26:18 +0100

    Add FGSM

---
+++ NIDS_attacks/fgsm.py
@@ -0,0 +1,28 @@
+import torch
+
+def attack_fgsm(model, criterion, x_val, target, top_features=None, eps = 0.01):
+    """
+    Args:
+        model 
+        criterion : loss function of the model
+        x_val
+        y_val : truth wanted
+        features : features we want to perturb
+        eps : range we are allowed to perturbate
+
+    Returns:
+        x_adv : 
+    """
+    x_adv = x_val.clone().detach().requires_grad_(True)
+
+    pred = model(x_adv)
+    loss = criterion(pred, target)
+
+    loss.backward()
+    with torch.no_grad():
+        if top_features is None:
+            x_adv = x_adv - eps * x_adv.grad.sign()
+        else:
+            x_adv[:, top_features] = x_adv[:, top_features] - eps * x_adv.grad[:, top_features].sign()
+    return x_adv
+    
