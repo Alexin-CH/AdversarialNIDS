569557a027b78868f746495be2e18c112af02d3d
Alexin-CH <alexin.choisnet@gmail.com>
2025-12-24 18:11:00 +0100

    Remove UNSWNB15

--- UNSWNB15/analysis/pca.py
+++
@@ -1,6 +0,0 @@
-from sklearn.decomposition import PCA
-
-def apply_pca(data, n_components=2):
-    pca = PCA(n_components=n_components)
-    principal_components = pca.fit_transform(data)
-    return principal_components, pca.explained_variance_ratio_
